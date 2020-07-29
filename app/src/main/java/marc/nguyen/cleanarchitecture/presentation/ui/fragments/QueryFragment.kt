dcecf523be3d23f82749c517887fdd1cd5237133
Nguyen Marc <nguyen_marc@live.fr>
2020-07-30 01:41:27 +0200

    Added Warnings

--- app/src/main/java/marc/nguyen/cleanarchitecture/presentation/ui/fragments/QueryFragment.kt
+++ app/src/main/java/marc/nguyen/cleanarchitecture/presentation/ui/fragments/QueryFragment.kt
@@ -1,13 +1,18 @@
 package marc.nguyen.cleanarchitecture.presentation.ui.fragments
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import marc.nguyen.cleanarchitecture.R
 import marc.nguyen.cleanarchitecture.databinding.QueryFragmentBinding
 import marc.nguyen.cleanarchitecture.presentation.viewmodels.QueryViewModel
 
@@ -24,7 +29,23 @@ class QueryFragment : Fragment() {
         binding.findButton.setOnClickListener {
             val user = viewModel.user.value
             if (!user.isNullOrBlank()) {
+                val inputMethodManager =
+                    context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
                 viewModel.navigateToGithub(user)
+            } else {
+                Toast.makeText(
+                    context,
+                    getString(R.string.username_is_empty_toast),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+        binding.repositoryInput.doAfterTextChanged {
+            if (viewModel.user.value.isNullOrBlank()) {
+                binding.repositoryInput.error = getString(R.string.username_is_empty_error)
+            } else {
+                binding.repositoryInput.error = null
             }
         }
         viewModel.navigateToGithub.observe(viewLifecycleOwner, Observer {
